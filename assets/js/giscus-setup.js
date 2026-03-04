4f2e66fa1c90ccf68a4ab9954b06563a83273340
umarquis <164336515+umarquis@users.noreply.github.com>
2026-03-04 11:07:27 +0000

    Deploying to gh-pages from @ umarquis/umarquis.github.io@da8923c35c586682734c0dfb93adbebc896f023e 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "umarquis/umarquis",
+    "data-repo": "umarquis/umarquis.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
