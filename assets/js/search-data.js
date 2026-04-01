87d8f0a9e557cce2ea59155522e8f5a3ceb0a36d
umarquis <164336515+umarquis@users.noreply.github.com>
2026-04-01 10:33:14 +0000

    Deploying to gh-pages from @ umarquis/umarquis.github.io@5b7a7ce691a586833c856127e8ecb7340a4904dc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -416,11 +416,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather.html";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2.html";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
